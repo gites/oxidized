0fa898667fc674db4080cfbdfb60a1ff10c1bd94
Tomasz Pawelczak <tomasz@pawelczak.eu>
2018-11-22 14:18:41 +0100

    fake version 0.24.3

--- lib/oxidized/version.rb
+++ lib/oxidized/version.rb
@@ -1,6 +1,6 @@
 module Oxidized
-  VERSION = '0.24.2'
-  VERSION_FULL = '0.24.2'
+  VERSION = '0.24.3'
+  VERSION_FULL = '0.24.3'
   def self.version_set
     version_full = %x(git describe --tags).chop rescue ""
     version      = %x(git describe --tags --abbrev=0).chop rescue ""
